b41d7318bd9f2e639d6bfec8ccd0b4749a425a9f
RuthvikC95 <54645098+RuthvikC95@users.noreply.github.com>
2019-11-17 18:23:18 -0500

    Updated logic (Commented)

--- CincinnatiAccidents/Pages/Index.cshtml.cs
+++ CincinnatiAccidents/Pages/Index.cshtml.cs
@@ -17,12 +17,50 @@ namespace CincinnatiAccidents.Pages
             using (var webClient = new WebClient())
             {
                 String trafficjsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/rvmt-pkmq.json");
-                var trafficAccidents = TrafficAccident.FromJson(trafficjsonString);
-                ViewData["trafficAccidents"] = trafficAccidents;
+              var trafficAccidents = TrafficAccident.FromJson(trafficjsonString);
+                QuickTypeTraffic.TrafficAccident [] trafficAccidents2 = QuickTypeTraffic.TrafficAccident.FromJson(trafficjsonString);
+               ViewData["trafficAccidents"] = trafficAccidents;
+
+              
+
+                IDictionary<string, QuickTypeTraffic.TrafficAccident> TrafficAccidentDictionary = new Dictionary<string, QuickTypeTraffic.TrafficAccident>();
+
+                /* foreach (QuickTypeTraffic.TrafficAccident accident in trafficAccidents)
+                 {
+                     TrafficAccidentDictionary.Add(accident.CpdNeighborhood, accident);
+                 } */ // Commenting this piece of code as there is an error while populating this "STRING" dictionary
 
                 String firejsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/vnsz-a3wp.json");
                 var fireAccidents = FireAccident.FromJson(firejsonString);
+                QuickTypeFire.FireAccident[] fireAccidents2 = QuickTypeFire.FireAccident.FromJson(firejsonString);
                 ViewData["fireAccidents"] = fireAccidents;
+
+                IDictionary<string, QuickTypeFire.FireAccident> FireAccidentDictionary = new Dictionary<string, QuickTypeFire.FireAccident>();
+
+
+                /* foreach (QuickTypeFire.FireAccident accident in fireAccidents)
+                 {
+                     FireAccidentDictionary.Add(accident.Neighborhood, accident);
+                 } */ // Commenting this piece of code as there is an error while populating this "STRING" dictionary
+
+
+                /*string neighborhoodjsonstring = webClient.DownloadString("https://cagisonline.hamilton-co.org/arcgis/rest/services/COUNTYWIDE/CagisCoreLayers/MapServer/11/query?where=1%3D1&outFields=*&outSR=4326&f=json");
+
+                QuickType.Attributes[] neighborhood  = QuickType.Attributes.FromJson(neighborhoodjsonstring);
+                
+                List<QuickType.Attributes> allneighborhoods = neighborhood.Nbdname;
+
+                List<string> Accidentproneneighborhoods = new List<string>();
+                       
+                foreach (QuickType.Attributes attribute in allneighborhoods)
+                {
+                    Console.WriteLine(attribute.nbdname);
+
+                       if (TrafficAccidentDictionary.ContainsKey(attribute.nbdname) || FireAccidentDictionary.ContainsKey(attribute.nbdname))
+                    {
+                        Accidentproneneighborhoods.Add(nbdname); 
+                    }
+                }*/ //This piece of code is intended to check if a neighborhood is safe or not based on its presence in the traffic accidents dataset and fire accidents dataset. However, for this code to work the dictionaries should be populated, first.
             }
         }
     }
