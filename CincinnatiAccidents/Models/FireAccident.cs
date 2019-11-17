1027b6c3e841d153eacef2529ab5c5c648464832
praneeth465 <praneeth465@gmail.com>
2019-11-17 16:02:43 -0500

    updated traffic accident.cs

--- CincinnatiAccidents/Models/FireAccident.cs
+++ CincinnatiAccidents/Models/FireAccident.cs
@@ -10,7 +10,7 @@ namespace QuickTypeFire
 {
     using System;
     using System.Collections.Generic;
-
+    using System.ComponentModel.DataAnnotations;
     using System.Globalization;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
@@ -29,6 +29,7 @@ namespace QuickTypeFire
         [JsonProperty("agency")]
         public Agency Agency { get; set; }
 
+        [DataType(DataType.Date)]
         [JsonProperty("create_time_incident")]
         public DateTimeOffset CreateTimeIncident { get; set; }
 
--- CincinnatiAccidents/Models/TrafficAccident.cs
+++ CincinnatiAccidents/Models/TrafficAccident.cs
@@ -10,7 +10,7 @@ namespace QuickTypeTraffic
 {
     using System;
     using System.Collections.Generic;
-
+    using System.ComponentModel.DataAnnotations;
     using System.Globalization;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
@@ -29,6 +29,7 @@ namespace QuickTypeTraffic
         [JsonProperty("cpd_neighborhood")]
         public string CpdNeighborhood { get; set; }
 
+        [DataType(DataType.Date)]
         [JsonProperty("crashdate")]
         public DateTimeOffset? Crashdate { get; set; }
 
@@ -81,7 +82,7 @@ namespace QuickTypeTraffic
         public string Weather { get; set; }
 
         [JsonProperty("zip", NullValueHandling = NullValueHandling.Ignore)]
-        public long? Zip { get; set; }
+        public string Zip { get; set; }
 
         [JsonProperty("unittype", NullValueHandling = NullValueHandling.Ignore)]
         public string Unittype { get; set; }
