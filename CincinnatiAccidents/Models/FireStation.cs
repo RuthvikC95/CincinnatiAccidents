cbca732bda523b01559f0cba0ec14a12988e6d3b
Kash496 <54644051+Kash496@users.noreply.github.com>
2019-12-03 11:22:31 -0500

    Adding CSS and JS

--- CincinnatiAccidents/Models/FireStation.cs
+++ CincinnatiAccidents/Models/FireStation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,13 @@ namespace CincinnatiAccidents.Models
     {
         public int Id { get; set; }
         public string Neighbourhood { get; set; }
+        [Display(Name = "Fire Station Number")]
         public string FireStationNumber { get; set; }
+        [Display(Name = "Engine Company")]
         public string EngineCompany {get; set;}
+        [Display(Name = "Ladder Company")]
         public string LadderCompany { get; set; }
+        [Display(Name = "Medical Unit")]
         public string MedicUnit { get; set; }
         public string District { get; set; }
         public string Zip { get; set; }
